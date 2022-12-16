81536a70d19e7f1314d9f1a22b28d050d5e10fb0
4pb <kg@gm.pl>
2022-12-16 09:49:55 +0100

    Lekcja03 - koniec

--- src/main/java/Car/CarUpdate.java
+++ src/main/java/Car/CarUpdate.java
@@ -1,4 +1,43 @@
 package Car;
 
-public record CarUpdate(String model, Integer year) {
+import java.util.Objects;
+
+public final class CarUpdate {
+    private String model;
+    private final Integer year;
+
+    public CarUpdate(String model, Integer year) {
+        this.model = model;
+        this.year = year;
+    }
+
+    public String model() {
+        return model;
+    }
+
+    public Integer year() {
+        return year;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (CarUpdate) obj;
+        return Objects.equals(this.model, that.model) &&
+                Objects.equals(this.year, that.year);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(model, year);
+    }
+
+    @Override
+    public String toString() {
+        return "CarUpdate[" +
+                "model=" + model + ", " +
+                "year=" + year + ']';
+    }
+
 }
